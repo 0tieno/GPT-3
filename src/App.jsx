07dfbb42f9440f9920b55d052cafd4a779066c1b
Sir Ronney Otieno <ronvi035@gmail.com>
2024-04-03 10:48:39 +0300

    initial file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import { Footer, Header, WhatGPT3, Features, Possibility, Blog } from './containers';
+
+import {Cta, Brand, Navbar} from './components';
+
+import './App.css';
+
+
 
 function App() {
   return (
 
-    <h1>GPT-3</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+
+       <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <Cta />
+        <Blog />
+        <Footer />
+    </div>
   );
 }
 
