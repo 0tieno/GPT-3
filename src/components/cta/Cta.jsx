ebec32f1a804249ce2ad49d4083b838c43e8dabb
Sir Ronney Otieno <ronvi035@gmail.com>
2024-04-03 10:17:07 +0300

    initial file setup

---
+++ src/components/cta/Cta.jsx
@@ -0,0 +1,10 @@
+import './cta.css';
+
+
+const Cta = () => {
+  return (
+    <div>CTA</div>
+  )
+}
+
+export default Cta
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import './navbar.css';	// Import css file
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
