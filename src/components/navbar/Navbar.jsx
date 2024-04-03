2b451335f4176c1053b0eafee86e4e55b7c387ba
Sir Ronney Otieno <ronvi035@gmail.com>
2024-04-03 13:38:09 +0300

    components structure

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,23 @@
 import './navbar.css';	// Import css file
+// import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';	// Import icons
+import logo from '../../assets/logo.svg';	// Import logo
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navabar-links_container">
+          <a href="#home">Home</a>
+          <a href="wgpt3">What is GPT3?</a>
+          <a href="#possibility">Open AI</a>
+          <a href="#features">Case studies</a>
+          <a href="#blog">Library</a>
+        </div>
+      </div>
+    </div>
   )
 }
 
